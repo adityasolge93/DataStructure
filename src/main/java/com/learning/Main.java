174f9ae5d6580f6841d4dbb4e70c540effc711b8
Aditya Solge <adityasolge@Adityas-MacBook-Pro.local>
2022-12-27 20:17:00 +0530

    Added test data and driver method to run sorting

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -2,16 +2,40 @@ package com.learning;
 
 import com.learning.sorting.BubbleSort;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] input = { 89, 68, 27, 12, 27, 67, 62, 87, 50, 35};
+        try {
+            Main main = new Main();
+            main.sortDriver();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    private void sortDriver() throws IOException {
+        String fileName = Objects.requireNonNull(
+                this.getClass().getClassLoader().getResource("random-numbers.txt")
+        ).getFile();
+
+        List<Integer> data = new ArrayList<>();
+        Scanner scanner = new Scanner(new File(fileName));
+        while (scanner.hasNextInt()) {
+            data.add(scanner.nextInt());
+        }
+
+        int[] input = data.stream().mapToInt(Integer::intValue).toArray();
         BubbleSort bubbleSort = new BubbleSort();
         int[] sorted = bubbleSort.sort(input);
 
-        Arrays.stream(sorted)
-                .forEach(i -> System.out.print(i + ", "));
+        System.out.println(Arrays.toString(sorted));
     }
 }
