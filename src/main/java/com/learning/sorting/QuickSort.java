78ed16f0f76aeafcda18b98a7ab852675d2db280
Aditya Solge <adityasolge@Adityas-MacBook-Pro.local>
2023-01-11 21:19:49 +0530

    Updating documentation

--- src/main/java/com/learning/sorting/QuickSort.java
+++ src/main/java/com/learning/sorting/QuickSort.java
@@ -1,9 +1,41 @@
 package com.learning.sorting;
 
+/**
+ * <b>Algorithm</b>
+ *      <p>In QuickSort, pivot element is chosen (it can be any element but usually first or last or middle element is
+ *      chosen as the pivot), and then the pivot is placed in its original place by putting all smaller elements to its
+ *      left and all greater elements to its right. The array is then divided into two arrays of smaller elements and
+ *      greater elements and the same procedure is applied on both the arrays.</p>
+ *
+ * <b>Pseudo Code: For putting pivot in its original position</b>
+ *      1.  int i=startIndex, j=endIndex-1, pivot=last element of the array
+ *      2.  while (i <= j)
+ *      3.      if (input[j] < pivot)
+ *      4.          swap (i, j)
+ *      5.          i++
+ *      5.      else
+ *      6.          j--
+ *      7.  end while
+ *      8.  swap (i, pivot)
+ *      10. Return i as the Paritition Point.
+ *
+ * <b>Pseudo Code: Recursive sorting</b>
+ *      1. if (startIndex > endIndex)
+ *      2.      return;
+ *      3. int partitionIndex = partition(input, start, end);
+ *      4. quicksort(input, startIndex, partitionIndex-1);
+ *      5. quicksort(input, partitionIndex+1, endIndex);
+ */
 public class QuickSort {
+
+    /**
+     * Method for performing the sorting.
+     *
+     * @param input the input array to sort.
+     * @return array with sorted values
+     */
     public int[] sort(int[] input) {
         quicksort(input, 0, input.length-1);
-
         return input;
     }
 
